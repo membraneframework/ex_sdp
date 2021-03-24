8cf57090f49f31869e3d757d0d01687e90204625
Jakub Perzylo <jakub.perzylo@swmansion.com>
2021-03-24 20:18:04 +0100

    Fixed variables pinning in tests

--- test/ex_sdp/attribute/msid_test.exs
+++ test/ex_sdp/attribute/msid_test.exs
@@ -7,14 +7,14 @@ defmodule ExSDP.Attribute.MSIDTest do
     test "parses msid without app data" do
       id = "0YiRg3sIeAEZEhwD3ANvRbn7UFf3BjYBeANS"
       assert {:ok, msid} = MSID.parse(id)
-      assert %MSID{id: id} = msid
+      assert %MSID{id: ^id} = msid
     end
 
     test "parses msid with app data" do
       id = "0YiRg3sIeAEZEhwD3ANvRbn7UFf3BjYBeANS"
       app_data = "a60cccca-f708-49e7-89d0-4be0524658a5"
       assert {:ok, msid} = MSID.parse("#{id} #{app_data}")
-      assert %MSID{id: id, app_data: app_data} = msid
+      assert %MSID{id: ^id, app_data: ^app_data} = msid
     end
 
     test "returns an error when id is empty" do
