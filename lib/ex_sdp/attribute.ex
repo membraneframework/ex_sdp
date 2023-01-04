e6837b2955e80d3393c0e51e809edf3136e4512c
blazpie <blazej.pierzak@gmail.com>
2023-01-04 10:47:08 +0000

    fix parsing type and orient attributes

--- lib/ex_sdp/attribute.ex
+++ lib/ex_sdp/attribute.ex
@@ -135,7 +135,7 @@ defmodule ExSDP.Attribute do
 
   defp parse_orient(orient) do
     case orient do
-      string when string in ["portrait", "landscape", "seascape"] -> String.to_atom(string)
+      string when string in ["portrait", "landscape", "seascape"] -> {:ok, {:orient, String.to_atom(string)}}
       _invalid_orient -> {:error, :invalid_orient}
     end
   end
@@ -143,7 +143,7 @@ defmodule ExSDP.Attribute do
   defp parse_type(type) do
     case type do
       string when string in ["broadcast", "meeting", "moderated", "test", "H332"] ->
-        String.to_atom(string)
+        {:ok, {:type, String.to_atom(string)}}
 
       _invalid_type ->
         {:error, :invalid_type}
