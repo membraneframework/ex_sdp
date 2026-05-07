fd96f1153f9384afe084b5cdd391b48c79922ad5
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2026-05-07 17:10:03 +0200

    Relax constraints of support dependencies

--- mix.exs
+++ mix.exs
@@ -43,8 +43,7 @@ defmodule ExSDP.MixProject do
 
   defp dialyzer() do
     opts = [
-      flags: [:error_handling],
-      plt_add_apps: [:mix, :syntax_tools]
+      flags: [:error_handling]
     ]
 
     if System.get_env("CI") == "true" do
@@ -71,9 +70,9 @@ defmodule ExSDP.MixProject do
     [
       {:bunch, "~> 1.3"},
       {:elixir_uuid, "~> 1.2"},
-      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.4", only: :dev, runtime: false},
-      {:credo, "~> 1.7", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
+      {:credo, ">= 0.0.0", only: :dev, runtime: false},
       {:mix_test_watch, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
